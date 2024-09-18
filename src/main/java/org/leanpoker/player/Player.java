ff591b29e8a72533b39e645b0294dae84fcf1070
Lada Kesseler <lada.kesseler1@t-mobile.com>
2024-09-18 08:22:12 -0700

    Back to folding

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,10 +4,10 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public class Player {
 
-    static final String VERSION = "v.1.0";
+    static final String VERSION = "v.1.1";
 
     public static int betRequest(JsonNode request) {
-        return 10;
+        return 0;
     }
 
     public static void showdown(JsonNode game) {

9092f64cea493660d0aff1485a310f7f4fe9fedb
Ralf Klemmer <r_klemmer@gmx.de>
2024-09-19 18:34:45 +0200

    Created empty PostFlopBiddingStrategy

--- src/main/java/org/leanpoker/player/domain/model/Player.java
+++ src/main/java/org/leanpoker/player/domain/model/Player.java
@@ -3,6 +3,7 @@ package org.leanpoker.player.domain.model;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.leanpoker.player.domain.BiddingStrategy;
 import org.leanpoker.player.domain.CardScorer;
+import org.leanpoker.player.domain.PostFlopBiddingStrategy;
 import org.leanpoker.player.presentation.parser.GameState;
 
 public class Player {
@@ -13,8 +14,14 @@ public class Player {
         var cards = state.getCards();
         var scorer = new CardScorer();
 
-        BiddingStrategy strategy = new BiddingStrategy(scorer, state.getCurrentBuyIn());
-        return strategy.decideBid(cards, state.getOurMoney());
+        var communityCards = state.getCommunityCards();
+        if (communityCards.isEmpty()) {
+            BiddingStrategy strategy = new BiddingStrategy(scorer, state.getCurrentBuyIn());
+            return strategy.decideBid(cards, state.getOurMoney());
+        } else {
+            PostFlopBiddingStrategy flopStrategy = new PostFlopBiddingStrategy(scorer);
+            return flopStrategy.decideBid(cards, communityCards, state.getOurMoney(), state.getCurrentBuyIn());
+        }
     }
 
     public static void showdown(JsonNode game) {
